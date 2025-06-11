c386463a4a017c29176e60e3277a7d7174a00162
lloydtl <thomaslloyd22@gmail.com>
2025-06-11 19:43:25 +0100

    updated token

--- api/contacts/index.ts
+++ api/contacts/index.ts
@@ -34,7 +34,7 @@ export default async function handler(req: any, res: any) {
     airtableData,
     {
       headers: {
-        Authorization: `Bearer ${airtableApiKey}`,
+        Authorization: `Bearer ${airtableToken}`,
         'Content-Type': 'application/json'
       }
     }
