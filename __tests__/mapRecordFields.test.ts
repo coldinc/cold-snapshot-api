3c0843f87aecd383d83abdb01b4432e68249ec00
coldinc <tom@cold.inc>
2025-06-16 12:57:59 +0100

    Add record field mapping utilities with tests

---
+++ __tests__/mapRecordFields.test.ts
@@ -0,0 +1,17 @@
+const { mapInternalToAirtable, mapAirtableToInternal } = require('../lib/mapRecordFields');
+
+describe('mapInternalToAirtable', () => {
+  it('maps internal keys to Airtable field names', () => {
+    const fieldMap = { name: 'Name', age: 'Age' };
+    const input = { name: 'Tom', age: 30, extra: 'ignore' };
+    expect(mapInternalToAirtable(input, fieldMap)).toEqual({ Name: 'Tom', Age: 30 });
+  });
+});
+
+describe('mapAirtableToInternal', () => {
+  it('maps Airtable record fields to internal keys and includes id', () => {
+    const fieldMap = { name: 'Name', age: 'Age', missing: 'Missing' };
+    const record = { id: 'rec1', fields: { Name: 'Tom', Age: 30 } };
+    expect(mapAirtableToInternal(record, fieldMap)).toEqual({ id: 'rec1', name: 'Tom', age: 30, missing: null });
+  });
+});
