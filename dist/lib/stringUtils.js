178870c3b76044aa387dfdbfc72bd53dae02381b
lloydtl <thomaslloyd22@gmail.com>
2025-06-17 17:12:28 +0100

    Clean up: remove dist folder to force fresh Vercel build

--- dist/lib/stringUtils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-// lib/stringUtils.ts
-var normalizeString = (str) => str.toLowerCase().replace(/[^a-z0-9]/g, "");
-var isMatch = (recordName, query) => normalizeString(recordName).includes(normalizeString(query));
-module.exports = { normalizeString, isMatch };
-//# sourceMappingURL=stringUtils.js.map
