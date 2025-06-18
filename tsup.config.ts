890ac72a33795bddc536597a00b9a61207f48558
coldinc <tom@cold.inc>
2025-06-18 21:34:45 +0100

    Configure build output for Vercel

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,13 +3,13 @@ import { join } from "path";
 
 export default defineConfig({
     entry: ["api/**/*.ts", "lib/**/*.ts", "utils/**/*.ts"],
-    outDir: ".",
+    outDir: "dist",
     format: ["esm"],
     target: "node20",
     dts: false,
     splitting: false,
     sourcemap: true,
-    clean: false,
+    clean: true,
     minify: false,
     shims: true,
     noExternal: ['uri-js', 'tr46', 'punycode'],
