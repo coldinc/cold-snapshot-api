31ea52085b51566350894ae3353cad2eca935589
lloydtl <thomaslloyd22@gmail.com>
2025-06-18 20:12:37 +0100

    fix(runtime): pin punycode to CJS v1.4.1

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from 'tsup'
-import { join } from 'path'
+import { defineConfig } from "tsup";
+import { join } from "path";
 
 export default defineConfig({
-  entry: ['api/**/*.ts', 'lib/**/*.ts', 'utils/**/*.ts'],
-  outDir: '.',
-  format: ['esm'],
-  target: 'node20',
-  dts: false,
-  splitting: false,
-  sourcemap: true,
-  clean: false,
-  minify: false,
-  shims: true,
-  esbuildOptions(options) {
-    options.external = options.external || []
-    options.external.push('openai') // Safely mark as external
-  },
-  alias: {
-    '@': join(__dirname, './')
-  }
-})
+    entry: ["api/**/*.ts", "lib/**/*.ts", "utils/**/*.ts"],
+    outDir: ".",
+    format: ["esm"],
+    target: "node20",
+    dts: false,
+    splitting: false,
+    sourcemap: true,
+    clean: false,
+    minify: false,
+    shims: true,
+    esbuildOptions(options) {
+        options.external = options.external || [];
+        options.external.push("openai"); // Safely mark as external
+    },
+    alias: {
+        "@": join(__dirname, "./")
+    }
+});
