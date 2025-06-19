1ad05531256e35ee90ae629efe7743522dd00e13
coldinc <tom@cold.inc>
2025-06-19 20:24:41 +0100

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ['src/api/**/*.ts'],
-  outDir: '.vercel/output/functions',
+  outDir: '/vercel/output/functions',
   clean: true,
   format: ['cjs'],
   target: 'node20',
