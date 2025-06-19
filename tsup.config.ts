3272e4201cf65dee41e885058b45f4adb7ca670a
coldinc <tom@cold.inc>
2025-06-19 20:05:40 +0100

    build(v3): generate Build-Output v3 bundle with nodejs20.x functions

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ['src/api/**/*.ts'],
-  outDir: 'api',
+  outDir: '.vercel/output/functions',
   clean: true,
   format: ['esm'],
   target: 'node20',
+  outbase: 'src',
   shims: false,
-  esbuildOptions(opts) {
-    opts.outbase = 'src/api';
-  },
-});
+  });
