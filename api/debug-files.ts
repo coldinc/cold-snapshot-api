7ab807156ab432a61e9b83d7622aeeb153f28ec9
coldinc <tom@cold.inc>
2025-06-20 16:44:42 +0100

    Update debug-files.ts

--- api/debug-files.ts
+++ api/debug-files.ts
@@ -1,5 +1,10 @@
 import fs from "fs";
+
 export default function handler(req: any, res: any) {
-  const apiFiles = fs.readdirSync(__dirname);
-  res.status(200).json({ apiFiles });
+  try {
+    const apiFiles = fs.readdirSync(__dirname);
+    res.status(200).json({ apiFiles });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message, stack: error.stack });
+  }
 }
